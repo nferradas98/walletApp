c32939337aa958065173e77e6c560e8c04f33090
Cristiano Caon <cristiano.caon@hotmail.com>
2020-02-11 16:26:18 -0600

    Update ControllerWithdraw.java

--- ControllerWithdraw.java
+++ ControllerWithdraw.java
@@ -1,19 +1,15 @@
 package sample;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class ControllerWithdraw {
     @FXML
-    public TextField accountName;
-    @FXML
     public TextField amountWithdraw;
-    @FXML
-    public Label updatedBalance;
 
     public void initialize() {
 
     }
 }
+
 
