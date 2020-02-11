b6902f8ad179bfa938da41dfad2a57594ba3fe29
Cristiano Caon <cristiano.caon@hotmail.com>
2020-02-11 16:24:16 -0600

    Update ControllerDeposit.java

--- ControllerDeposit.java
+++ ControllerDeposit.java
@@ -1,16 +1,12 @@
 package sample;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class ControllerDeposit {
-    @FXML
-    public TextField accountName;
+
     @FXML
     public TextField amountDeposit;
-    @FXML
-    public Label updatedBalance;
 
     public void initialize() {
 
