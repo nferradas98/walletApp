85b3bf86db4446551a9f8ada843ac283f1dc1046
NickLovera <60933011+NickLovera@users.noreply.github.com>
2020-02-12 15:32:04 -0600

    Update ControllerDeposit.java
    Added Deposit Function

--- ControllerDeposit.java
+++ ControllerDeposit.java
@@ -11,4 +11,22 @@ public class ControllerDeposit {
     public void initialize() {
 
     }
+    
+    public boolean deposit(){
+        //Setting transaction details
+		deposit.setType("Deposit");
+		deposit.setAccount(account());
+		deposit.setAmount(amountDeposit);
+		//Does the user enter details?
+		deposit.setDetails("Deposited $" + amount);
+		//Any of yall know hot to get it as a string i can get it in type 'Date' that can
+		//be printed but not passed as a String
+		deposit.setDate("2/8/20");
+	    //Date date = Calendar.getInstance.getTime();
+        
+        return Datasource.getInstance.saveTransaction(deposit);
+    }
+    
+    private Account account(){
+        return Datasource.getInstance.account();
 }
